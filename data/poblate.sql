01447ae4b935645b537f0cbc2ff8a5d9e817b6ad
Javier Lopez <javierlopm@hotmail.com>
2015-05-13 19:02:51 -0430

    Poblando bd y settings de bd

---
+++ data/poblate.sql
@@ -0,0 +1,16 @@
+INSERT INTO "Departaments" values (1,'Informacion'     );
+INSERT INTO "Departaments" values (2,'Recursos Humanos');
+INSERT INTO "Departaments" values (3,'Desarrollo'      );
+
+INSERT INTO "Roles" values(1,'Developer'     );
+INSERT INTO "Roles" values(2,'Scrum Master'  );
+INSERT INTO "Roles" values(3,'Product Owner' );
+
+INSERT INTO "Users" VALUES('Javier'      ,'javierlopm' ,'holahola1','javier@correo.com'  ,1,1);
+INSERT INTO "Users" VALUES('Nabil'       ,'nabilj'     ,'holahola2','nabil@correo.com'   ,2,1);
+INSERT INTO "Users" VALUES('Cristina'    ,'cristinab'  ,'holahola3','cristina@correo.com',3,1);
+INSERT INTO "Users" VALUES('Samuel'      ,'samuel'     ,'holahola4','samuel@correo.com'  ,1,1);
+INSERT INTO "Users" VALUES('Roberto'     ,'robertor'   ,'holahola5','roberto@correo.com' ,2,1);
+INSERT INTO "Users" VALUES('Meggie'      ,'meggies'    ,'holahola6','meggie@correo.com'  ,3,1);
+INSERT INTO "Users" VALUES('Ascander'    ,'ascander'   ,'holahola7','ascander@correo.com',1,3);
+INSERT INTO "Users" VALUES('Jean Carlos' ,'jeanc'      ,'holahola8','jeanc@correo.com'   ,2,2);
