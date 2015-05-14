a40abd6d080d88dfaa3f0985945d770db051d573
Javier Lopez <javierlopm@hotmail.com>
2015-05-14 18:23:39 -0430

    Base.py con base de datos, comando createdb, migrates. Version funcional

--- base.py
+++ base.py
@@ -2,13 +2,19 @@ from flask import Flask, request, session
 from flask.ext.script import Manager, Server
 from random import SystemRandom
 from datetime import timedelta
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__, static_url_path='')
 manager = Manager(app)
 manager.add_command("runserver", Server(
     use_debugger = True,
     use_reloader = True,
-    host = '0.0.0.0')
+    host = '127.0.0.1')
 )
 
 @app.before_request
@@ -20,6 +26,20 @@ def make_session_permanent():
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@manager.command
+def createdb():
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import create_engine
+    from sqlalchemy.engine.url import URL
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(URL(**app.config['DATABASE']))
+    db.metadata.drop_all(engine)
+    db.metadata.create_all(engine)
+
+    DBSession = sessionmaker(bind = engine)
+    session = DBSession()
 
 from app.scrum.ident import ident
 app.register_blueprint(ident)
@@ -36,15 +56,102 @@ app.register_blueprint(objetivo)
 from app.scrum.accion import accion
 app.register_blueprint(accion)
 
-
-
 #Application code starts here
 
+app.config['DATABASE'] = {
+            'drivername': 'postgres',
+            'host': 'localhost',
+            'port': '5432',
+            'username': 'apmwsc',
+            'password': 'safepassword',
+            'database': 'apmwsc'
+            }
 
-#Application code ends here
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+#Clase para pila de productos
+
+class Producto(db.Model):
+    __tablename__ = 'Productos'
+    idProducto    = db.Column(Integer, primary_key = True) #autoincremento
+    descripcion   = db.Column(String(500),  unique = True)
+
+    #Backrefs
+    # roles      = relationship('ProductRoles'     ,backref='Products')
+    # actions    = relationship('ProductActions'   ,backref='Products')
+    # objectives = relationship('ProductObjectives',backref='Products')
+
+    def __init__(self,descripcion):
+        self.description = descripcion
+
+# Clase Accion
+
+class Accion(db.Model):
+    
+    __tablename__ = 'Acciones'
+    idAccion      = db.Column(db.Integer, primary_key = True)
+    descripcion   = db.Column(db.String(500), unique = True)
+    idProducto    = db.Column(db.Integer, db.ForeignKey('Productos.idProducto'))
+    producto      = db.relationship('Producto', backref = db.backref('acciones', lazy = 'dynamic'))
+    
+    ''' Metodo init
+        Constructor de accion
+    ''' 
+    
+    def __init__(self, idAccion, descripcion, idProducto):
+        
+        self.idAccion = idAccion
+        self.descripcion = descripcion
+        self.idProducto = idProducto
+
+# Clase Actor
+
+class Actor(db.Model):
+    
+    __tablename__ = 'Actores'
+    idActor = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(50), unique = True)
+    descripcion = db.Column(db.String(500), unique = True)
+    idProducto = db.Column(db.Integer, db.ForeignKey('Productos.idProducto'))
+    producto = db.relationship('Producto', backref = db.backref('actores', lazy = 'dynamic'))
+    ''' Metodo init
+        Constructor del actor
+    ''' 
+    
+    def __init__(self, idActor, nombre, descripcion, idProducto):
+        
+        self.idActor = idActor
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.idProducto = idProducto
+     
+# Clase Usuario
+
+class Objetivo(db.Model):
+    
+    __tablename__ = 'Objetivos'
+    idObjetivo = db.Column(db.Integer, primary_key = True)
+    descripcion = db.Column(db.String(500), unique = True)
+    idProducto = db.Column(db.Integer, db.ForeignKey('Productos.idProducto'))
+    producto = db.relationship('Producto', backref = db.backref('objetivos', lazy = 'dynamic'))
+    
+    ''' Metodo init
+        Constructor del objetivo
+    ''' 
+    
+    def __init__(self, idObjetivo, descripcion, idProducto):
+        
+        self.idObjetivo = idObjetivo
+        self.descripcion = descripcion
+        self.idProducto = idProducto
+
 
 if __name__ == '__main__':
     app.config.update(
       SECRET_KEY = repr(SystemRandom().random())
     )
     manager.run()
+
