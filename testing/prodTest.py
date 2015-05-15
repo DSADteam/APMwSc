31bf2481f722c902b24bc9000bed67973c69720e
Javier Lopez <javierlopm@hotmail.com>
2015-05-15 12:35:45 -0430

    Estructura de testing creado

--- testing/prodTest.py
+++ testing/prodTest.py
@@ -3,4 +3,10 @@ import os
 dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '../..'))
 sys.path.append(dir)
 
+from app.scrum.prod import clsProducto
 import base
+import unittest
+
+class MdlTest(unittest.TestCase):
+    def setUp(self):
+        self.prod=clsProducto(engine,sessionDB)
