199809abca8880b945886f797b2412e13aafac5a
Javier Lopez <javierlopm@hotmail.com>
2015-05-16 19:48:52 -0430

    Poblate adaptado a clave autoincremental. En bd ids principales de tablas eliminados

--- data/poblate.sql
+++ data/poblate.sql
@@ -14,14 +14,14 @@ INSERT INTO "Productos" (nombre,descripcion) VALUES ('Surfeadores divertido','De
 INSERT INTO "Productos" (nombre,descripcion) VALUES ('Piedreros asociados','Adoro esta materia');
 INSERT INTO "Productos" (nombre,descripcion) VALUES ('Ingenieria de software','Ojala la abrieran todos los trimestres');
 
-INSERT INTO "Acciones" VALUES (1,'Crear repositorio',1);
-INSERT INTO "Acciones" VALUES (2,'Documentar todo',1);
+INSERT INTO "Acciones" (descripcion,"idProducto") VALUES ('Crear repositorio',1);
+INSERT INTO "Acciones" (descripcion,"idProducto") VALUES ('Documentar todo',1);
 
-INSERT INTO "Acciones" VALUES (3,'Crear repositorio divertido',2);
-INSERT INTO "Acciones" VALUES (4,'Documentar tablas',2);
+INSERT INTO "Acciones" (descripcion,"idProducto") VALUES ('Crear repositorio divertido',2);
+INSERT INTO "Acciones" (descripcion,"idProducto") VALUES ('Documentar tablas',2);
 
-INSERT INTO "Acciones" VALUES (5,'Crear repositorio volador',3);
-INSERT INTO "Acciones" VALUES (6,'Documentar cosas',3);
+INSERT INTO "Acciones" (descripcion,"idProducto") VALUES ('Crear repositorio volador',3);
+INSERT INTO "Acciones" (descripcion,"idProducto") VALUES ('Documentar cosas',3);
 
 INSERT INTO "Actores" (nombre,descripcion,"idProducto") VALUES ('Developer','Desarrolador de proyecto',1);
 INSERT INTO "Actores" (nombre,descripcion,"idProducto") VALUES ('Scrum Master','Guia del grupo',1);
@@ -35,9 +35,9 @@ INSERT INTO "Actores" (nombre,descripcion,"idProducto") VALUES ('Developer','Des
 INSERT INTO "Actores" (nombre,descripcion,"idProducto") VALUES ('Scrum Master','Guia del grupo',3);
 INSERT INTO "Actores" (nombre,descripcion,"idProducto") VALUES ('Product Owner','Dueno del producto',3);
 
-INSERT INTO "Objetivos" VALUES (1,'En tanto que hagamos codigo lo continuemos',1);
-INSERT INTO "Objetivos" VALUES (2,'En tanto que todos nos divirtamos todo sera divertido',1);
-INSERT INTO "Objetivos" VALUES (3,'En tanto que jejeps todos huehuehue',1);
-INSERT INTO "Objetivos" VALUES (4,'En tanto que escribo esto nabil hace prod.py',2);
-INSERT INTO "Objetivos" VALUES (5,'En tanto que probamos la base de datos aprendemos',2);
-INSERT INTO "Objetivos" VALUES (6,'En tanto que jejeps todos huehuehue',2);
+INSERT INTO "Objetivos" (descripcion,"idProducto") VALUES ('En tanto que hagamos codigo lo continuemos',1);
+INSERT INTO "Objetivos" (descripcion,"idProducto") VALUES ('En tanto que todos nos divirtamos todo sera divertido',1);
+INSERT INTO "Objetivos" (descripcion,"idProducto") VALUES ('En tanto que jejeps todos huehuehue',1);
+INSERT INTO "Objetivos" (descripcion,"idProducto") VALUES ('En tanto que escribo esto nabil hace prod.py',2);
+INSERT INTO "Objetivos" (descripcion,"idProducto") VALUES ('En tanto que probamos la base de datos aprendemos',2);
+INSERT INTO "Objetivos" (descripcion,"idProducto") VALUES ('En tanto que jejeps todos huehuehue',2);
