cf474aa478ded1f771fb6d4461ea3914e4ee0c51
Javier Lopez <javierlopm@hotmail.com>
2015-05-27 20:02:27 -0430

    Inclusion de nueva interfaz de historias. Todos los cambios anteriores siguen funcionando

--- app/scrum/historias.py
+++ app/scrum/historias.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
-
-#Agregando proyect root
-import sys
-import os
-dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '../../..'))
-sys.path.append(dir)
-
-#Dependencias flask
 from flask import request, session, Blueprint, json
-from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql.expression import text
 
-#Definicion de blueprint y bd
 historias = Blueprint('historias', __name__)
-from base import *
+
 
 @historias.route('/historias/ACrearHistoria', methods=['POST'])
 def ACrearHistoria():
@@ -25,14 +12,8 @@ def ACrearHistoria():
     res = results[0]
     #Action code goes here, res should be a list with a label and a message
 
-    idPila = str(session['idPila'])
-    session.pop("idPila",None)
-    
-    his = clsHistoria(session=sessionDB,engine=engine)
-    his.insertar(descripcion=params['descripcion'],idProducto=idPila)
-    
     #Datos de prueba
-    res['label'] = res['label'] + '/' + idPila
+    res['label'] = res['label'] + '/1'
 
     #Action code ends here
     if "actor" in res:
@@ -52,12 +33,8 @@ def AModifHistoria():
     res = results[0]
     #Action code goes here, res should be a list with a label and a message
 
-    idPila = int(request.args.get('idPila', 1))
-    his = clsHistoria(session=sessionDB,engine=engine)
-    his.modificar(int(request.args.get('idPila', 1)),params['descripcion'])
-
-    #Datos de prueba    
-    res['label'] = res['label'] + '/' + str(idPila)
+    #Datos de prueba
+    res['label'] = res['label'] + '/1'
 
     #Action code ends here
     if "actor" in res:
@@ -66,6 +43,7 @@ def AModifHistoria():
         else:
             session['actor'] = res['actor']
     return json.dumps(res)
+
 
 
 @historias.route('/historias/VCrearHistoria')
@@ -97,12 +75,12 @@ def VCrearHistoria():
       {'key':1,'value':'Opcional'},
       {'key':2,'value':'Obligatoria'}]
     res['fHistoria'] = {'super':0}
-    res['idPila'] = 1
 
-    session['idPila'] = request.args['idPila']
-    
+
     #Action code ends here
     return json.dumps(res)
+
+
 
 @historias.route('/historias/VHistoria')
 def VHistoria():
@@ -134,18 +112,11 @@ def VHistoria():
       {'key':2,'value':'Obligatoria'}]
     res['fHistoria'] = {'super':0, 
        'actor':1, 'accion':2, 'objetivo':3, 'tipo':1} 
-    res['idPila'] = 1
 
 
-    his = clsHistoria(engine=engine,session=sessionDB)
-    idPila = int(request.args.get('idPila', 1))
-
-    pilas = his.listarHistorias()
-    res['fHistoria'] = pilas[idPila-1]
-    
-    idHistoria = idPila
     #Action code ends here
     return json.dumps(res)
+
 
 
 @historias.route('/historias/VHistorias')
@@ -160,92 +131,15 @@ def VHistorias():
     res['data0'] = [
       {'idHistoria':1, 'enunciado':'En tanto que picho podría tomar agua para saciar mi sed'}]
     
-    session['idPila'] = request.args['idPila']
 
     #Action code ends here
     return json.dumps(res)
 
 
+
+
+
 #Use case code starts here
 
-class clsHistoria():
-    
-    def __init__(self,engine=None,session=None):
-        
-        self.engine  = engine
-        self.session = session
-        
-    def insertar(self,descripcion=None,idProducto=None):
-        
-        comentarioNulo = (descripcion == None) or\
-        (idProducto)==None
-        if comentarioNulo:
-            return False
-
-        estaEnBd       = self.existeHistoria(descripcion=descripcion)
-        #pr = clsProducto()
-        #estaEnBd = estaEnBd and pr.existeProducto(idProducto)
-        longCharValido = (len(descripcion) <= 500)
-
-        if (not estaEnBd) and (longCharValido) and (not comentarioNulo):
-            newHis = Historia(descripcion,idProducto)
-            self.session.add(newHis)
-            self.session.commit()
-            return True
-        else:
-            return False
-        
-    def existeHistoria(self,descripcion=None):
-        
-        if(descripcion!=None):
-            result  = self.engine.execute("select * from \"Historias\" where \'descripcion\'=\'"+descripcion+"\';")
-        else:
-            return False
-        
-        contador = 0
-        for row in result:
-            contador += 1
-
-        return contador != 0
-
-    def listarHistorias(self):
-        
-        res = []
-        result = self.engine.execute("select * from \"Historias\";")
-        if result!="":
-            for row in result:
-                res.append({'idHistoria':row.idHistoria,'descripcion':row.descripcion})
-            else:
-                print("Empty query!")
-                
-    def listarHistoriasprod(self,idProducto):
-        
-        res = []
-        #result = self.engine.execute("select * from \"Historias\" where idProducto= "+str(idProducto)+" ;")
-        result = self.session.query(Historia).filter(Historia.idProducto == idProducto)
-        if result!="":
-            for row in result:
-                res.append({'idHistoria':row.idHistoria,'descripcion':row.descripcion})
-            else:
-                print("Empty query!")
-        
-        return res
-    
-    def borrarFilas(self):
-        
-        self.session.query(Historia).delete()
-        self.session.commit()
-
-    #Funcion que permite actualizar la descripcion
-    def modificar(self,id=None,descripcion=None):
-        
-        if id and descripcion:
-            
-            self.session.query(Historia).filter(Historia.idHistoria == id).\
-                update({'descripcion' : descripcion })
-            self.session.commit()
-            return True
-        else:
-            return False
 
 #Use case code ends here
