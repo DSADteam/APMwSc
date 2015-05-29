b1b2b0591fb65f978825685cbcd21bad3cb9eb96
root <root@nutella>
2015-05-28 20:25:55 -0430

    Funcionan todos los tests excepto los de historias

--- base.py
+++ base.py
@@ -181,38 +181,28 @@ class Historia(db.Model):
     idAccion        = db.Column(Integer, db.ForeignKey('Acciones.idAccion'), nullable=True)
     idHistoriaPadre = db.Column(Integer, db.ForeignKey('Historias.idHistoria'), unique = False, nullable=True)
     
-<<<<<<< HEAD
     # Backrefs
     
-    accion          = db.relationship('Acciones', backref = db.backref('accion', lazy = 'dynamic'))
-    producto        = db.relationship('Productos', backref = db.backref('producto', lazy = 'dynamic'))
-    historia        = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
-=======
+    #accion          = db.relationship('Acciones', backref = db.backref('accion', lazy = 'dynamic'))
+    #producto        = db.relationship('Productos', backref = db.backref('producto', lazy = 'dynamic'))
+    #historia        = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
+
     #Backrefs
-<<<<<<< HEAD
+
     # accion          = db.relationship('Acciones',   backref = db.backref('accion'   , lazy = 'dynamic'))
     # producto        = db.relationship('Productos', backref = db.backref('producto', lazy = 'dynamic'))
     # historia   = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
->>>>>>> 80ed37eeb8223ed24c07a7398ced3a4fe8553997
-=======
+
     #accion          = db.relationship('Acciones',   backref = db.backref('accion'   , lazy = 'dynamic'))
     #producto        = db.relationship('Productos', backref = db.backref('producto', lazy = 'dynamic'))
     #historia   = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
 
->>>>>>> origin/Cristina
-
     ''' Metodo init
         Constructor de las historias de usuarios
     ''' 
-    
-<<<<<<< HEAD
-    def __init__(self,codigo,idProducto,idAccion,idHistoriaPadre=None):
 
-        self.codigo      = codigo
-=======
     def __init__(self,codigo, idProducto,idAccion,tipo,idHistoriaPadre=None):
         self.codigo = codigo
->>>>>>> origin/Cristina
         self.idProducto  = idProducto
         self.idAccion    = idAccion
         self.tipo        = tipo
@@ -227,18 +217,15 @@ class ObjetivosHistoria(db.Model):
     idHistoria    = db.Column(Integer, db.ForeignKey('Historias.idHistoria'), unique = False, primary_key=True)
     idObjetivo    = db.Column(Integer, db.ForeignKey('Objetivos.idObjetivo'), unique = False, primary_key=True)   
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    objetivo      = db.relationship('Objetivo', backref = db.backref('objetivo', lazy = 'dynamic'))
-    historia      = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
-=======
+    #objetivo      = db.relationship('Objetivo', backref = db.backref('objetivo', lazy = 'dynamic'))
+    #historia      = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
+
     # objetivo   = db.relationship('Objetivo', backref = db.backref('objetivo'  , lazy = 'dynamic'))
     # historia   = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
->>>>>>> 80ed37eeb8223ed24c07a7398ced3a4fe8553997
-=======
+
+
     #objetivo   = db.relationship('Objetivo', backref = db.backref('objetivo'  , lazy = 'dynamic'))
     #historia   = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
->>>>>>> origin/Cristina
 
     ''' Metodo init
         Constructor de Objetivos asociados a Historias
@@ -257,18 +244,15 @@ class ActoresHistoria(db.Model):
     idHistoria      = db.Column(Integer, db.ForeignKey('Historias.idHistoria'), unique = False, primary_key=True)
     idActor         = db.Column(Integer, db.ForeignKey('Actores.idActor'),     unique = False,  primary_key=True)
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-    actor           = db.relationship('Actor', backref = db.backref('actor'   , lazy = 'dynamic'))
-    historia        = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
-=======
+    #actor           = db.relationship('Actor', backref = db.backref('actor'   , lazy = 'dynamic'))
+    #historia        = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
+
     # actor           = db.relationship('Actor',   backref = db.backref('actor'   , lazy = 'dynamic'))
     # historia        = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
->>>>>>> 80ed37eeb8223ed24c07a7398ced3a4fe8553997
-=======
+
     #actor           = db.relationship('Actor',   backref = db.backref('actor'   , lazy = 'dynamic'))
     #historia        = db.relationship('Historias', backref = db.backref('historia', lazy = 'dynamic'))
->>>>>>> origin/Cristina
+
 
     ''' Metodo init
         Constructor de Actores asociados a Historias
